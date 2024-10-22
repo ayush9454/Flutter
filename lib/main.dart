16a3954bcbb32125e519f15543e2be871b5686a7
AYUSH <singhayush9454@gmail.com>
2024-10-22 20:56:20 +0530

    22th october

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 void main() {
-  //print("Hello World");
-  String? someValue;
+  // //print("Hello World");
+  // String? someValue;
 
-  someValue = null;
-  print(someValue?.length??0);
+  // someValue = null;
+  // print(someValue?.length ?? 0);
+
+  // const age = "28!18";
+  // if (age.endsWith("!")) {
+  //   print("Adult");
+  // } else {
+  //   print("Child");
+  // }
+  // //Ternery
+  // print(age.startsWith("1") ? "Wow" : "naha");
+  // var address = "ABC";
+  // int shippingCost = 10;
+  // switch (address) {
+  //   case "XYZ":
+  //     print(shippingCost * 5);
+  //   case "ABC":
+  //     print(shippingCost * 7);
+  //   case "PRQ":
+  //     print(shippingCost * 10);
+  //   default:
+  //     print("Error ! ");
+  //}
+
+  for (int i = 0; i < 10; i++) {
+    print("This is ${i+1} ");
+  }
 }
